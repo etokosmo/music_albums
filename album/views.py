754c81cd2aba28198aba6536d3b2bf5062acbf62
Denis Polyakov <p-ov.da@ya.ru>
2022-11-02 00:20:53 +0300

    Save data in db from POST API

--- album/views.py
+++ album/views.py
@@ -1,11 +1,19 @@
+import re
+
 import requests
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from .models import Album
+from .models import Album, Artist, Track
 from .serializers import AlbumSerializer
+
+
+def get_year_by_album(album: str) -> int:
+    pattern = r'\[(\d+)\]'
+    year = re.search(pattern, album).group(1)
+    return int(year)
 
 
 @api_view(['GET', 'POST'])
@@ -18,6 +26,19 @@ def handle_album(request):
     if request.method == 'POST':
         serializer = AlbumSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
+        artist, created = Artist.objects.get_or_create(
+            name=serializer.validated_data['artist@name']
+        )
+        album = Album.objects.create(
+            name=serializer.validated_data['name'],
+            artist=artist,
+            year=get_year_by_album(serializer.validated_data['album'])
+        )
+        for track in serializer.validated_data['tracks']:
+            Track.objects.create(
+                name=track,
+                album=album
+            )
         return Response(serializer.validated_data)
 
 
