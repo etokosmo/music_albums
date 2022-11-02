5701f1ce5359ef7d8a4ae3a26bbb3803a21402b8
Denis Polyakov <p-ov.da@ya.ru>
2022-11-02 11:08:02 +0300

    Add default SECRET_KEY

--- music_albums/settings.py
+++ music_albums/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY', 'replace_me')
 DEBUG = env.bool('DEBUG', True)
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost'])
 
